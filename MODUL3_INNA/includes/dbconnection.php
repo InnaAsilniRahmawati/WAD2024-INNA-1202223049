24dc8fd19ffd180fcb1593d0851bd899cd88fac4
InnaAsilniRahmawati <innaasilni18@gmail.com>
2024-11-16 11:34:14 +0700

    MODUL 3

---
+++ MODUL3_INNA/includes/dbconnection.php
@@ -0,0 +1,14 @@
+<?php
+// DB credentials as variables
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'uwibu';
+
+// Buatkan koneksi ke database
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
